72b18cd2032a97a0b999103852ce14c1c49ffdcd
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-04-16 17:48:58 -0700

    save

--- recipes/recipesModel.js
+++ recipes/recipesModel.js
@@ -4,6 +4,23 @@ const find = () => {
   return db('recipes');
 }
 
+const getShoppingList = id => {
+  return db('Ingredients')
+    .where('recipe_id', '=', id)
+    .then(items => {
+      console.log(items)
+    })
+}
+
+const getInstructions = id => {
+  return db('Steps')
+    .where('recipe_id', '=', id)
+    .then(steps => {
+      console.log(steps)
+    })
+}
+
+getInstructions(1)
 module.exports = {
   find
 }
